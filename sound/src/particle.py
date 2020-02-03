794daa71acf2f3adb5e2e9aebf431d09fb5fb175
rotifyld <dawidborys98@gmail.com>
2020-02-03 20:46:41 +0100

    cleanup, colors!

--- sound/src/particle.py
+++ sound/src/particle.py
@@ -5,35 +5,8 @@ import pygame as pg
 
 from src.config import Config
 
-
 # todo colors
-# colors = [(255, 255, 0), (0, 255, 255), (255, 0, 255)]
-
-
-class Particle:
-    def __init__(self):
-        self.x = random() * Config.box.width
-        self.y = random() * Config.box.height
-        self.vx = (random() - 0.5) * 2 * Config.particle.v_max
-        self.vy = (random() - 0.5) * 2 * Config.particle.v_max
-
-
-def bounce_walls(x, y, vx, vy):
-    # bounce off walls
-    if x < 0:
-        x = -x
-        vx = -vx
-    elif x > Config.game.width:
-        x = 2 * Config.game.width - x
-        vx = -vx
-    if y < 0:
-        y = -y
-        vy = -vy
-    elif y > Config.game.height:
-        y = 2 * Config.game.height - y
-        vy = -vy
-
-    return x, y, vx, vy
+colors = [(255, 255, 0), (0, 255, 255), (255, 0, 255)]
 
 
 class Particles:
@@ -44,11 +17,12 @@ class Particles:
         self.y = np.random.sample(n) * Config.box.height
         self.vx = 2 * (np.random.sample(n) - 0.5) * Config.particle.v_max
         self.vy = 2 * (np.random.sample(n) - 0.5) * Config.particle.v_max
+        self.colors = [colors[randint(0, len(colors) - 1)] for _ in range(n)]
 
     def draw(self):
-        for x, y in zip(self.x, self.y):
+        for x, y, color in zip(self.x, self.y, self.colors):
             pg.draw.circle(self.display,
-                           Config.particle.color,
+                           color,
                            (int(x), int(y)),
                            Config.particle.r)
 
